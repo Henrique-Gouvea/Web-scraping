24d9be57bb6607622b1efc5ffd1e160b1a2b7e6c
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-11-08 19:56:10 -0300

    feat(search_engine): mudando nome list_tuple

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -2,7 +2,7 @@ from tech_news.database import search_news
 from datetime import datetime
 
 
-def list_for_tuple(news_contain_title):
+def list_tuple(news_contain_title: list):
     return [(new["title"], new["url"]) for new in news_contain_title]
 
 
@@ -10,17 +10,16 @@ def search_by_title(title: str) -> list:
     news_contain_title: list = search_news(
         {"title": {"$regex": title, "$options": "i"}}
     )
-    return list_for_tuple(news_contain_title)
+    return list_tuple(news_contain_title)
 
 
-# Requisito 7
 def search_by_date(date: str) -> list:
     try:
         date_formated = datetime.fromisoformat(date).strftime("%d/%m/%Y")
         news_contain_date: list = search_news(
             {"timestamp": {"$eq": date_formated}}
         )
-        return list_for_tuple(news_contain_date)
+        return list_tuple(news_contain_date)
     except ValueError:
         raise ValueError("Data inválida")
 
