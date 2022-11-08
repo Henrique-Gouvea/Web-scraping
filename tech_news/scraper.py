664d5b2e7b5640b118e321b69dfb78ad3826506d
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-11-08 17:40:34 -0300

    feat(scraper): Retornando um dict com os dados da noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -24,9 +24,24 @@ def scrape_next_page_link(html_content) -> list:
     return selector.css("a.next::attr(href)").get()
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    comment = selector.css("h5.title-block::text").get()
+    quantity_comments = (
+        comment.split()[0] if (comment.split()[0] != "Arquivos") else 0
+    )
+
+    return {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        # rstrip cópia da string com os caracteres finais removidos.
+        "title": selector.css("h1.entry-title::text").get().rstrip(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("a.url::text").get(),
+        "comments_count": quantity_comments,
+        "summary": selector.xpath("string(//p)").get().rstrip(),
+        "tags": selector.css(".post-tags a::text").getall(),
+        "category": selector.css("span.label::text").get(),
+    }
 
 
 # Requisito 5
